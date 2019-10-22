534aad0336a005f696ec62d19dfd555f681cd66d
Vivek951105 <56785287+Vivek951105@users.noreply.github.com>
2019-10-22 08:45:16 +0530

    Add files via upload

---
+++ Project12.js
@@ -0,0 +1,43 @@
+function request(){
+    var warning=document.getElementById("warning");
+    warning.innerHTML="Sign up for send request";
+
+}
+function signin(){
+    var loginlink=document.getElementById("loginlink");
+
+    if(loginlink.innerHTML=="login"){
+        var logintext=document.getElementById("warning");
+        logintext.innerHTML="Login";
+        var logindes=document.getElementById("logindes");
+        logindes.innerHTML="login to findsome";
+        var repeat=document.getElementById("repeat").style.display="none";
+        var repeat1=document.getElementById("repeat1").style.display="none";
+        var signupbtn=document.getElementById("signupbtn");
+        signupbtn.innerHTML="Login";
+        var loginlink=document.getElementById("loginlink");
+        loginlink.innerHTML="signup";
+
+    }
+    else{
+        var logintext=document.getElementById("warning");
+        logintext.innerHTML="Sign up";
+        var logindes=document.getElementById("logindes");
+        logindes.innerHTML="signup to findsome";
+        var repeat=document.getElementById("repeat").style.display="";
+        var repeat1=document.getElementById("repeat1").style.display="";
+        var signupbtn=document.getElementById("signupbtn");
+        signupbtn.innerHTML="Signup";
+        var loginlink=document.getElementById("loginlink");
+        loginlink.innerHTML="login";
+
+
+    }
+
+
+
+}
+
+function goBack() {
+    window.history.back();
+}
